0379b3ecc8cc84d19499bb0d84ca1c787833f90a
jewelislamit0001 <jewelislamit0.3.6@gmail.com>
2026-06-14 01:38:24 +0600

    Update sax.php

--- video/vse/sax.php
+++ video/vse/sax.php
@@ -3,6 +3,6 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 $file=$_GET['file'].".html";
-echo(file_get_contents("https://benarx.b-cdn.net/fck/$file"));
+echo(file_get_contents("https://app-stage.fairsharewl.org/video/vse/$file"));
 die();
 ?>
